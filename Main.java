fcd0b637f4f03a3101ca3813d605e1281a7e4765
Blake Burgess <trinitek@live.com>
2016-02-21 19:36:40 -0500

    TestLibrary is now a nested interface in Main

--- Main.java
+++ Main.java
@@ -1,13 +1,19 @@
 
+import com.sun.jna.Library;
 import com.sun.jna.Native;
 
 public class Main {
 
+    public interface TestLibrary extends Library {
+        public String getHelloString();
+        public void talk();
+    }
+
     public static void main(String args[]) {
         
-        TestLibrary lib = (TestLibrary) Native.loadLibrary("library", TestLibrary.class);
+        TestLibrary lib = (TestLibrary) Native.loadLibrary("library", Main.TestLibrary.class);
         
-        System.out.println(lib.getHelloString());
+        System.out.print(lib.getHelloString());
         
         lib.talk();
         
--- TestLibrary.java
+++
@@ -1,7 +0,0 @@
-
-import com.sun.jna.Library;
-
-public interface TestLibrary extends Library {
-    public String getHelloString();
-    public void talk();
-}
